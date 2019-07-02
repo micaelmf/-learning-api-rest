99c269603ab5c605ded7d42c7aa4ad9141379120
Micael Ferreira <micaelmf@gmail.com>
2019-07-02 10:46:30 -0300

    creating edit, list and remove Veterinary

--- project/src/Controller/VeterinaryController.php
+++ project/src/Controller/VeterinaryController.php
@@ -36,10 +36,59 @@ class VeterinaryController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($vet);
             $entityManager->flush();
+
+            return $this->redirectToRoute('vet_edit',[
+                'id' => $vet->getId()
+            ]);
         }
         
         return $this->render('veterinary/new-veterinary.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    public function list()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $query = $entityManager->createQuery('SELECT veterinary FROM App\Entity\Veterinary veterinary');
+        $vets = $query->getResult();
+
+        return $this->render('veterinary/list-veterinary.html.twig', [
+            'vets' => $vets,
+        ]);
+    }
+
+    public function edit(Request $request, Veterinary $vet)
+    {
+        $form = $this->createForm(VeterinaryType::class, $vet)
+            ->add('save', SubmitType::class, ['label' => 'Confirm']);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $vet = $form->getData();
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('vet_edit',[
+                'id' => $vet->getId()
+            ]);
+        }
+        
+        return $this->render('veterinary/edit-veterinary.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function remove(Request $request, Veterinary $vet)
+    {
+        $entityManager = $this->getDoctrine('App\Entity\Veterinary')->getManager();
+        $entityManager->remove($vet);
+        $entityManager->flush();
+        
+        return $this->render('veterinary/remove-veterinary.html.twig',[
+            'vet' => $vet,
         ]);
     }
 }
